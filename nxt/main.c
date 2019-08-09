54f1ef22c2e09ebf499bfc88b3f3357d952ec537
Laurens Valk <laurens@pybricks.com>
2019-08-09 19:55:23 +0200

    javavm: Drop calls to Java VM
    This takes the binary size from 53K down to about 10K.

--- nxt/main.c
+++ nxt/main.c
@@ -42,12 +42,6 @@
 
 #include <string.h>
 
-unsigned int gNextProgramSize;
-byte *gNextProgram;
-unsigned int gProgramExecutions=0;
-
-byte *region;
-
 void
 shutdown()
 {
@@ -151,157 +145,18 @@ switch_thread_hook()
     buttonsDown = false;
 }
 
-#ifdef VERIFY
-void
-assert_hook(boolean aCond, int aCode)
-{
-  if (aCond) return;
-  display_reset();
-  display_goto_xy(0,0);
-  display_string("Assert failed");
-  display_goto_xy(0,1);
-  display_string("Code: ");
-  display_goto_xy(6,1);
-  display_int(aCode,0);
-  display_update();
-  while(1); // Hang
-}
-#endif
-
-/**
- * Request that a new program be started.
- */
-int
-run_program(byte *start, unsigned int len)
-{
-  if (!is_valid_executable(start, len)) return -1;
-  gNextProgram = start;
-  gNextProgramSize = len;
-  schedule_request(REQUEST_EXIT);
-  return 0;
-}
-
-
-void
-run(int jsize)
-{
-  byte *ram_end = (byte *) (&__free_ram_end__);
-  byte *ram_start = (byte *) (&__free_ram_start__);
-
-  init_events();
-  init_debug();
-
-  //printf("Initializing Binary\n");
-
-  // Init the static storage and class sync objects in ram
-  {
-    MasterRecord *mrec = get_master_record();
-    int staticSize = mrec->staticStateLength;
-    int syncSize = (mrec->lastClass + 1) * sizeof(objSync);
-
-#if ! EXECUTE_FROM_FLASH
-    // Skip java binary if it is an top of ram
-    if (jsize > 0)
-      ram_end -= (jsize + 4);
-#endif
-
-    staticSize = (staticSize + 3) & ~(3);
-    syncSize = (syncSize + 3) & ~(3);
-  
-    ram_end -= staticSize;
-    classStaticStateBase = ram_end;
-    memset( (byte *)classStaticStateBase, 0, staticSize);
-    ram_end -= syncSize;
-    staticSyncBase = (objSync *)ram_end;
-    memset( (byte *)staticSyncBase, 0, syncSize);
-
-#if EXECUTE_FROM_FLASH
-    // When we execute from flash we need extra storage for the class state.
-    int statusSize = (mrec->lastClass + 1) * sizeof( classStatusBase[0]);
-    statusSize = (statusSize + 3) & ~(3);
-    ram_end -= statusSize;
-    classStatusBase = ram_end;
-    memset( (byte *)classStatusBase, 0, statusSize);
-#endif
-  }
-
-  // Initialize binary image state
-  initialize_binary();
-
-  //printf("Initializing memory\n");
-
-  // Initialize memory
-  memory_init();
-  region = ram_start;
-  memory_add_region(region, (byte *) ram_end);
-
-  //printf("Initializing exceptions\n");
-
-  // Create the execution environment and boot thread
-  init_threads();
-  // Initialize exceptions
-  init_exceptions();
-
-  // Execute the bytecode interpreter
-  engine();
-  // Engine returns when all non-daemon threads are dead
-}
-
 /***************************************************************************
  * int nxt_main *--------------------------------------------------------------------------
  ***************************************************************************/
 //int main (int argc, char *argv[])
 int
-nxt_main(const byte *bin, int size)
+nxt_main()
 {
-  int jsize = 0;
-  const byte *binary; 
-
-#if EXECUTE_FROM_FLASH
-  if (bin != NULL) {
-    size = (size + 3) & ~3;
-    binary = bin;
-    jsize = size - 4;
-  } else {
-    // Execute flash menu
-
-    bin = (byte *) menu_address;
-    size = menu_length;
-    size = (size + 3) & ~3;
-    binary = bin;
-    jsize = size - 4;
-  }
-#else
-  unsigned *temp;
-  if (bin != NULL) {
-    size = (size + 3) & ~3;
-    temp = ((unsigned *) (&__free_ram_end__)) - (size >> 2);
-    memcpy(temp,bin,size);
-    binary = (byte *) temp;
-    jsize = size - 4;
-  } else {
-    // Execute flash menu
-
-    bin = (byte *) menu_address;
-    size = menu_length;
-    size = (size + 3) & ~3;
-    temp = ((unsigned *) (&__free_ram_end__)) - (size >> 2);   
-    memcpy(temp,bin,size);
-    binary = ((byte *) temp);
-    jsize = size - 4;
-  }
-#endif
-  
-  // reset all motors, sensors and devices
-
   sp_init();
   display_set_auto_update_period(DEFAULT_UPDATE_PERIOD);
-  //       printf("Installing Binary\n");
 
-  install_binary(binary);
+  // Run user app
 
-  //      printf("Running\n");
-  run(jsize);
   init_events();
   display_reset();
   nxt_motor_reset_all();
@@ -314,47 +169,13 @@ nxt_main(const byte *bin, int size)
   return 0;
 }
 
-#ifdef FW_SPLASH
-const U8 splash_data[4 * 26] = {
-  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-  0x00, 0x3F, 0x3F, 0x3F, 0x3F, 0x3F, 0x3F, 0xFF, 0xFF, 0xFF, 0xFF, 0xFE, 0xFC,
-  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xA0, 0x40, 0xA0, 0x40,
-  0xA0, 0x40, 0xA0, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
-  0xF0, 0xF0, 0xF0, 0xF0, 0xF0, 0xF0, 0x00, 0x00, 0x00, 0x0A, 0x05, 0x0A, 0x05,
-  0x0A, 0x05, 0x0A, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
-  0x0F, 0x1F, 0x3F, 0x3F, 0x7F, 0x7F, 0xFC, 0xF8, 0xF0, 0xF0, 0xF0, 0xF0, 0xF0,
-  0xF0, 0xF0, 0xF0, 0xF0, 0xF0, 0xF8, 0xFC, 0x7F, 0x7F, 0x3F, 0x3F, 0x1F, 0x0F,
-};
-
-void
-show_splash()
-{
-  display_clear(0);
-  display_bitmap_copy(splash_data, 26, 4, 37, 1);
-
-  display_goto_xy(6, 6);
-  display_string("LEJOS");
-  display_update();
-}
-#endif
-
-
-#if 0
-void
-xx_show(void)
+void device_test(void)
 {
   int iterator = 0;
   U32 buttons;
   U32 motor_mode = 0;
   int result;
-  U8 distance;
 
-  show_splash(3000);
-
-  /* set up power for ultrasonic sensor on port 1 (port 0 here )*/  
-  nxt_avr_set_input_power(0,1);
-  i2c_enable(0);
-                        
   while (1) {
     display_clear(0);
 
@@ -412,14 +233,6 @@ xx_show(void)
       display_unsigned(sensor_adc(2), 5);
       display_unsigned(sensor_adc(3), 5);
       
-      i2c_start_transaction(0,1,0x42,1,&distance,1,0);
-      systick_wait_ms(200);
-      result = i2c_busy(0);
-
-      display_goto_xy(0,6);
-      display_string("DIST ");
-      display_unsigned(distance,3);
-
       display_update();
       systick_wait_ms(500);
       
@@ -432,9 +245,6 @@ xx_show(void)
       display_string("TIME ");
       display_unsigned(systick_get_ms(), 0);
       display_goto_xy(0, 2);
-      display_string("Stack ");
-      display_unsigned(free_stack(), 0);
-      display_goto_xy(0, 3);
       switch (motor_mode) {
       case 0:
 	display_string("MOTORS REV");
@@ -462,7 +272,6 @@ xx_show(void)
     }
   }
 }
-#endif
 
 void
 main(void)
@@ -484,16 +293,9 @@ main(void)
   udp_init();
   systick_wait_ms(1000); // wait for LCD to stabilize
   display_init();
-#ifdef FW_SPLASH
-  show_splash(); 
-#endif
-  gNextProgram = NULL;
   do 
   {
-  	byte *next = gNextProgram;
-  	gNextProgram = NULL;
-  	gProgramExecutions++;
-  	nxt_main(next, gNextProgramSize);
+  	nxt_main();
   }
   while (true);
 
